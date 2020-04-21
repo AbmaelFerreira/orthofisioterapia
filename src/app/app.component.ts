5178998bac6be2ef92b5cee5f273fa69182afaf9
AbmaelFerreira <abmael_ninha@hotmail.com.br>
2020-04-21 01:04:12 -0400

    Novo layout 210420

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from '../../node_modules/jquery/dist/jquery.min.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+
+    $(document).ready(function(){
+        $('.menu').click(function(){
+            $('ul').toggleClass('active');
+        })
+    })
+   
+  }
   title = 'orthofisioterapia';
 }
