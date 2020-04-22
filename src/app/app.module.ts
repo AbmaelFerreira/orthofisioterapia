66ad289414667da485d4a3dbefb280de2d5c4d5a
AbmaelFerreira <abmael_ninha@hotmail.com.br>
2020-04-21 23:29:43 -0400

    Layout Verde 210420

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobreComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
